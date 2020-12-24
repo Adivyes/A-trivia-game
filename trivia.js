2ea401cf867246321a4630dc0578a1b7492b34e0
Adivyes <Adivyes@gmail.com>
2020-12-24 23:29:50 +0200

    init

--- trivia.js
+++ trivia.js
@@ -1,18 +1,18 @@
 
 let trivia = new Map()
-trivia.set('Animal',['dog','cat','hors','bear','mouse'])
-trivia.set('Computer',['macbook','lenovo','razer','dell','hp'])
-trivia.set('Food',['pizza','falafel','pasta','injera','magloba'])
-trivia.set('Geography',['ethiopia','israel','arizona','jordan','netherland'])
-trivia.set('Movie',['tenet','Inception','matrix','interstellar','parasite'])
+trivia.set('Animal', ['dog', 'cat', 'hors', 'bear', 'mouse'])
+trivia.set('Computer', ['macbook', 'lenovo', 'razer', 'dell', 'hp'])
+trivia.set('Food', ['pizza', 'falafel', 'pasta', 'injera', 'magloba'])
+trivia.set('Geography', ['ethiopia', 'israel', 'arizona', 'jordan', 'netherland'])
+trivia.set('Movie', ['tenet', 'Inception', 'matrix', 'interstellar', 'parasite'])
 
 let question = new Map()
-question.set("question",['What animal has claws?','Well known for gamers is?','Goes well with with sigha is?','The hottest place is?','The best movie is?'])
+question.set("question", ['What animal has claws?', 'Well known for gamers is?', 'Goes well with with sigha is?', 'The hottest place is?', 'The best movie is?'])
 
 let counter = 0
 
 
-for(const item of trivia.keys() ){
+for (const item of trivia.keys()) {
   // יצרתי לולאה שרצה על כל הקיים של המפה טריוויה 
   //לאחר מכן צירפתי לכל קי מהמפה
   //י1- דיב שמכיל בתוכו פונקציית און-מאוס-אובר
@@ -23,16 +23,17 @@ for(const item of trivia.keys() ){
         <div id="${item}Values" style="display:none"></div>
    </div>`
 
-   let myvalues = trivia.get(item)
-   
-   myvalues.forEach(element => {
-    
-      let categoryObjDiv =  document.getElementById(`${item}Values`)
-       categoryObjDiv.innerHTML += `<h4 onclick="answerCheck('${element}','${item}')">${element}</h4>`;
-   });
-   document.getElementById(`${item}Values`).innerHTML += `<span id="${item}question" style="display:none; font-weight: bolder">${question.get('question')[counter]}</span>`
-   counter++
-    }
+  let myvalues = trivia.get(item)
+
+  myvalues.forEach(element => {
+
+    let categoryObjDiv = document.getElementById(`${item}Values`)
+    categoryObjDiv.innerHTML += `<h4 onclick="answerCheck('${element}','${item}')">${element}</h4>`;
+  });
+  document.getElementById(`${item}Values`).innerHTML += 
+  `<span id="${item}question" style="display:none; font-weight: bolder">${question.get('question')[counter]}</span>`
+  counter++
+}
 //עם פונקציי  הגיט משכנו את הערכים של כל קי מהמפה (כשהם בצורת מערך)ושייכנו אותם למשתנה "הערכים שלי"י
 //על המשתנה "הערכים שלי" הפעלנו לולאת פור איץ' שהיא מכניסה כל ערך לתגית אייץ'-4 שכולם מוכנסים לקי שלהם (לדיב שייצרנו בהתחלה עם הסטייל של דיספליי נאן)
 //תגית האייץ'-4 מכילה בתוכה פונקצייה של און-קליק שתאפשר לנו בהמשך לבחור תשובה בסוויץ' קייס
@@ -42,40 +43,40 @@ for(const item of trivia.keys() ){
 
 
 function keyValDisplay(key1) {
-  for(const triviaKeyArray of trivia.keys()){
+  for (const triviaKeyArray of trivia.keys()) {
     document.getElementById(`${triviaKeyArray}Values`).style.display = "none"
   }
-  let categoryObjDiv =  document.getElementById(`${key1}Values`)
+  let categoryObjDiv = document.getElementById(`${key1}Values`)
   categoryObjDiv.style.display = "block";
 }
 
-function questionDisplay(questionKey){
-  for(const triviaKeyArray of trivia.keys()){
+function questionDisplay(questionKey) {
+  for (const triviaKeyArray of trivia.keys()) {
     document.getElementById(`${triviaKeyArray}question`).style.display = "none"
   }
   document.getElementById(`${questionKey}question`).style.display = "block"
 
 }
 
-function answerCheck(values,keys) {
+function answerCheck(values, keys) {
   switch (values) {
     case 'bear':
-    
-      return  answerTrivia.innerHTML = 'You dare to pic bear. verey good!!'
+
+      return answerTrivia.innerHTML = 'You dare to pic bear. verey good!!'
 
     case 'razer':
-      return  answerTrivia.innerHTML = 'If you chose razer you are gamer. verey good!!'
-      
+      return answerTrivia.innerHTML = 'If you chose razer you are gamer. verey good!!'
+
     case 'injera':
-      return  answerTrivia.innerHTML = 'Food in computer memory I will take terra injera. verey good!!'
+      return answerTrivia.innerHTML = 'Food in computer memory I will take terra injera. verey good!!'
 
     case 'arizona':
-      return  answerTrivia.innerHTML = 'The whole point is to drink a Coca-Cola in the heart of Arizona. verey good!!'
+      return answerTrivia.innerHTML = 'The whole point is to drink a Coca-Cola in the heart of Arizona. verey good!!'
 
     case 'parasite':
-      return  answerTrivia.innerHTML = 'All parasites live on stereotypes. verey good!!'
+      return answerTrivia.innerHTML = 'All parasites live on stereotypes. verey good!!'
 
     default:
-      return  answerTrivia.innerHTML = 'You can keep trying!!'
+      return answerTrivia.innerHTML = 'You can keep trying!!'
   }
 }
